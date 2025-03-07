d8b42f824522d3e1f37d6d55d53c04808a955f24
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-03-07 23:16:43 +0530

    Remove Movie page import from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
-import { Movie } from "./pages/Movie";
 import { Contact } from "./pages/Contact";
 import { AppLayout } from "./components/layouts/AppLayout";
 import { Product } from "./pages/Product";
